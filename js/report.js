0dcd754aea5fca4ee5c0c0ec5c9da7042186d573
algoritmokrasisoftware <algoritmokrasi@gmail.com>
2025-12-23 17:13:33 +0300

    Create report.js

---
+++ js/report.js
@@ -0,0 +1,11 @@
+export function downloadJSON(obj, filename){
+  const blob = new Blob([JSON.stringify(obj, null, 2)], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+}
